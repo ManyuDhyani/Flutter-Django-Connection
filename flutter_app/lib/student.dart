a89dcbdbfe7de31b30cb5a85b038c5aff89296f7
ManyuDhyani <manyudhyani@gmail.com>
2022-02-16 23:00:19 +0530

    Missing } in sudent.dart

--- flutter_app/lib/student.dart
+++ flutter_app/lib/student.dart
@@ -3,20 +3,18 @@ import 'package:http/http.dart' as http;
 
 class Student {
   String baseURL = "http://127.0.0.1:8000/api/student/";
-  Future<List> getAllStudent() async{
-    try{
+  Future<List> getAllStudent() async {
+    try {
       var response = await http.get(Uri.parse(baseURL));
-      if(response.statusCode == 200){
+      if (response.statusCode == 200) {
         return jsonDecode(response.body);
-      }else{
+      } else {
         return Future.error("Server Error");
       }
-    }
-    catch (e) {
+    } catch (e) {
       return Future.error(e);
     }
   }
+}
 
-
-
-//"http://localhost:8000/api/student/"
+// "http://localhost:8000/api/student/"
